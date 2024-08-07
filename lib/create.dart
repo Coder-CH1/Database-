644ed21d9ab2f2135d28cadc14f2e1a84aa3c06c
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-08-07 12:19:01 +0100

    added

--- lib/create.dart
+++ lib/create.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
 
 class CreatePage extends StatefulWidget {
 
@@ -9,7 +11,8 @@ class CreatePage extends StatefulWidget {
 }
 
 class _CreatePageState extends State<CreatePage> {
-  final TextEditingController _controller = TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,16 +38,16 @@ class _CreatePageState extends State<CreatePage> {
               color: Colors.white54,
             )),
             SizedBox(
-              height: 20,
+              height: 10,
             ),
             TextField(
               keyboardType: TextInputType.name,
-              controller: _controller,
+              controller: _titleController,
               maxLength: 20,
               style: TextStyle(
-                fontSize: 18),
+                  fontSize: 18),
               decoration: InputDecoration(
-                hintText: 'Category name',
+                labelText: 'Category name',
                 border: OutlineInputBorder(),
                 enabledBorder: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.white30),
@@ -54,17 +57,39 @@ class _CreatePageState extends State<CreatePage> {
 
               },
             ),
-            Text('Category icon', style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.normal,
-              color: Colors.white54,
-            )
-            ),
             SizedBox(
               height: 20,
             ),
+            Text('Category date', style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.normal,
+              color: Colors.white54,
+            )),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              keyboardType: TextInputType.datetime,
+              controller: _dateController,
+              maxLength: 20,
+              style: TextStyle(
+                  fontSize: 18),
+              decoration: InputDecoration(
+                labelText: 'Category date',
+                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white30),
+                ),
+              ),
+              onChanged: (value) {
+
+              },
+            ),
             ElevatedButton(
-                child: Text('Choose icon from library', style: TextStyle(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white10,
+                ),
+                child: Text('Create', style: TextStyle(
                   fontSize: 10,
                   fontWeight: FontWeight.normal,
                   color: Colors.white54,
@@ -72,13 +97,40 @@ class _CreatePageState extends State<CreatePage> {
                 onPressed: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const CreatePage()),
+                    MaterialPageRoute(
+                      builder: (context) => TaskList(
+                        title: _titleController.text,
+                        date: _dateController.text,
+                      ),
+                    ),
                   );
-                }
+                },
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+class TaskList extends StatelessWidget {
+  final String title;
+  final String date;
+  const TaskList({Key? key, required this.title, required this.date}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
+class TaskBox extends StatelessWidget {
+  const TaskBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
---
+++ lib/database.dart
@@ -0,0 +1,23 @@
+import 'package:sqflite/sqflite.dart';
+
+Future<Database> createDatabase() async {
+  return await openDatabase(
+    'task.db',
+    version: 1,
+    onCreate: (db, version) {
+      db.execute('''
+        CREATE TABLE task (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        date TEXT
+        )
+      ''');
+    }
+  );
+}
+
+Future<List<Map<String, dynamic>>>
+getTasks() async {
+  final db = await createDatabase();
+  return await db.query('task');
+}
