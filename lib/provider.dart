48162659ef92d927ee2804a0dcc1e338b1a96cc1
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-08-07 19:35:57 +0100

    added

---
+++ lib/provider.dart
@@ -0,0 +1,25 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseProvider with ChangeNotifier {
+
+}
+
+//CRUD OPERATIONS
+
+Future<void>createData() async {
+
+}
+
+Future<void>readData() async {
+
+}
+
+Future<void>updateData() async {
+
+}
+
+Future<void>deleteData() async {
+
+}
