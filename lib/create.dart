aec98172ec72324f866a0cfb55d89d8667a4e84f
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-08-07 11:05:58 +0100

    added

--- lib/create.dart
+++ lib/create.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'main.dart';
 
 class CreatePage extends StatefulWidget {
 
@@ -14,16 +13,19 @@ class _CreatePageState extends State<CreatePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white30,
+        title: Text('Create new category', style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.normal,
+          color: Colors.white54,
+        )),
+      ),
       backgroundColor: Colors.black,
       body: Padding(
         padding: const EdgeInsets.only(top: 50),
         child: Column(
           children: [
-            Text('Create new category', style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.normal,
-              color: Colors.white54,
-            )),
             SizedBox(
               height: 15,
             ),
@@ -32,21 +34,48 @@ class _CreatePageState extends State<CreatePage> {
               fontWeight: FontWeight.normal,
               color: Colors.white54,
             )),
+            SizedBox(
+              height: 20,
+            ),
             TextField(
+              keyboardType: TextInputType.name,
               controller: _controller,
               maxLength: 20,
               style: TextStyle(
                 fontSize: 18),
               decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(width: 2, color: Colors.white30)
+                hintText: 'Category name',
+                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white30),
                 ),
-                hintText: ''
               ),
               onChanged: (value) {
 
               },
+            ),
+            Text('Category icon', style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.normal,
+              color: Colors.white54,
             )
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                child: Text('Choose icon from library', style: TextStyle(
+                  fontSize: 10,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white54,
+                )),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const CreatePage()),
+                  );
+                }
+            ),
           ],
         ),
       ),
