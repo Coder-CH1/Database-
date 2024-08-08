3388f02cae5df6efe7205d97e47e7556bfd05ee9
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-08-08 13:00:03 +0100

    added

--- lib/provider.dart
+++ lib/provider.dart
@@ -3,38 +3,44 @@ import 'package:flutter/material.dart';
 import 'model.dart';
 
 class DatabaseProvider with ChangeNotifier {
+  Database? _database;
 
   Future<Database> getDb() async {
     if (_database != null) return _database!;
     _database = await initDatabase();
     return _database!;
   }
-  Future<Database> initDatabase() async {
-  return await openDatabase(
-    'task.db',
-    version: 1,
-    onCreate: (db, version) {
 
-    },
-  );
+  Future<Database> initDatabase() async {
+    return await openDatabase(
+      'task.db',
+      version: 1,
+      onCreate: (db, version) {
+
+      },
+    );
   }
-}
-Database? _database;
+
 //CRUD OPERATIONS
 
-Future<Task> createTask(Task task) async {
-  final db = await _database;
-return task;
-}
+  Future<Task> createTask(Task task) async {
+    final db = await getDb();
+    final id = await db.insert('task', task.toJson());
+    return Task(id: id, title: task.title, date: task.date);
+  }
 
-Future<List<Task>> readData() async {
-return [];
-}
+  Future<List<Task>> readData() async {
+    final db = await getDb();
+    final tasks = await db.query('task');
+    return tasks.map((task) => Task.fromJson(task)).toList();
+  }
 
-Future<Task> updateData(Task task) async {
-return task;
-}
+  Future<Task> updateData(Task task) async {
+    return task;
+  }
 
-Future<bool> deleteData(int id) async {
-return true;
+  Future<bool> deleteData(int id) async {
+    return true;
+  }
+
 }
