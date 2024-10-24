079cda5fc3d0d8742fc8c1b74a7811502799532d
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-10-24 10:45:22 +0100

    added

--- lib/create.dart
+++ lib/create.dart
@@ -1,6 +1,7 @@
 import 'package:database/database.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'main.dart';
 import 'model.dart';
 
 class CreatePage extends StatefulWidget {
@@ -165,58 +166,6 @@ class _CreatePageState extends State<CreatePage> {
           ],
         ),
       ),
-    );
-  }
-}
-
-//
-class TaskList extends StatelessWidget {
-  final List<Tasks> tasks;
-  const TaskList({Key? key, required this.tasks}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-  appBar: AppBar(
-backgroundColor: Colors.black26,
-  ),
-      backgroundColor: Colors.black87,
-      body: GridView.builder(
-     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-         crossAxisCount: 2,
-       childAspectRatio: 2.5,
-     ),
-        itemCount: tasks.length,
-        itemBuilder: (context, index) {
-       return ListView(
-         children: [
-           TaskBox(
-             tasks[index].title,
-             tasks[index].date,
-           ),
-         ]
-       );
-        },
-      ),
-    );
-  }
-}
-
-//
-class TaskBox extends StatelessWidget {
-  final String title;
-  final DateTime date;
-  const TaskBox(this.title, this.date, {Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text('$title - ${DateFormat('dd MMMM yyyy').format(date)}', style: const TextStyle(
-        fontSize: 14,
-        color: Colors.white,
-      ),
-      ),
-      tileColor: Colors.blueAccent,
     );
   }
 }
